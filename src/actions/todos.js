52ec9b1320a0be8e04b870f6fb466c74ee700e82
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-10-28 22:55:29 +0300

    Actions

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -4,21 +4,21 @@ export const ADD_TODO = 'ADD_TODO'
 export const REMOVE_TODO = 'REMOVE_TODO'
 export const TOGGLE_TODO = 'TOGGLE_TODO'
 
-function addTodoAction (todo) {
+function addTodo (todo) {
     return {
         type: ADD_TODO,
         todo
     }
 }
 
-function removeTodoAction (id) {
+function removeTodo (id) {
     return {
         type: REMOVE_TODO,
         id
     }
 }
 
-function toggleTodoAction (id) {
+function toggleTodo (id) {
     return {
         type: TOGGLE_TODO,
         id
@@ -27,11 +27,11 @@ function toggleTodoAction (id) {
 
 export function handleDeleteToDo (todo) {
     return (dispatch) => {
-        dispatch(removeTodoAction(todo.id));
+        dispatch(removeTodo(todo.id));
 
         return API.deleteTodo(todo)
             .catch(() => {
-                dispatch(addTodoAction(todo));
+                dispatch(addTodo(todo));
                 alert('An error occurred. Try again.');
             });
     }      
@@ -41,7 +41,7 @@ export function handleAddTodo (name, cb) {
     return (dispatch) => {
         return API.saveTodo(name)
                 .then((todo) => {
-                    dispatch(addTodoAction(todo));
+                    dispatch(addTodo(todo));
                     cb();
                 })
                 .catch(() => alert('An error occurred. Try again.'))
@@ -50,11 +50,11 @@ export function handleAddTodo (name, cb) {
 
 export function handleToggleTodo (id) {
     return (dispatch) => {
-        dispatch(toggleTodoAction(id));
+        dispatch(toggleTodo(id));
 
         return API.saveTodoToggle(id)
             .catch(() => {
-                dispatch(toggleTodoAction(id));
+                dispatch(toggleTodo(id));
                 alert('An error occurred. Try again.');
             });
     }
